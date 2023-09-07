16649b164b93d17863ac7454950a8973af7e6cb9
pharmacistismail <pharmacist.77@outlook.com>
2023-09-07 15:47:05 -0700

    .

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main() {
+    // Use the puts function to print the text
+    puts("Programming is like building a multilingual puzzle");
+
+    // End the program with the value 0
+    return 0;
+}
