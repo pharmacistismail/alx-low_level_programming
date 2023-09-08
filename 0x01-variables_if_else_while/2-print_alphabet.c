fd9aaf64630de6e298796b48a42d6a83ea1e35f1
pharmacistismail <pharmacist.77@outlook.com>
2023-09-08 15:15:58 -0700

    .

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
+
 /**
  * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
  */
+int main(void)
+{
+	char letter;
 
-int main() {
-    char ch;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-    for (ch = 'a'; ch <= 'z'; ch++) {
-        putchar(ch);
-    }
+	putchar('\n');
 
-    putchar('\n');
-
-    return 0;
+	return (0);
 }
