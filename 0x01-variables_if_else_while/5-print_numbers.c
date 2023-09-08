5fcedec56d69ecd5a9c73abadc1505b3de0e38eb
pharmacistismail <pharmacist.77@outlook.com>
2023-09-08 15:44:57 -0700

    .

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,27 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
-#include<stdio.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* 
- * the main function to print digit numbers
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
  *
- *
- * the output always 0
- * */
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int num;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-
-	for (num=0; num < 10; num++)
+	for (num = 0; num < 10; num++)
 		printf("%d", num);
 
 	printf("\n");
-
 
 	return (0);
 }
