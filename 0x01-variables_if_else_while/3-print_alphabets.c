48832edd0889ee4c00388d92f170c2124d9ec160
pharmacistismail <pharmacist.77@outlook.com>
2023-09-08 15:27:41 -0700

    .

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,12 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
-/* more headers goes there */
-
-/* the code prints 
- * 
- * the alphabet in lowercase,
- *  and then in uppercase
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
  *
- *  */
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
