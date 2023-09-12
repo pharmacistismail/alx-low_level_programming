de5005a8a0bedb4049ea1458d76755ba2daf59bd
pharmacistismail <pharmacist.77@outlook.com>
2023-09-12 08:49:37 -0700

    .

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,3 @@
-main.h
-
-#ifndef main_h
-#define main_h
-
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -16,6 +11,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-#endif
 
