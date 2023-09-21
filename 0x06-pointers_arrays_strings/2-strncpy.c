e6b6d49dd61a08e38fda225c77e981c65f254865
pharmacistismail <pharmacist.77@outlook.com>
2023-09-20 19:41:19 -0700

    .

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,12 @@
-#include"main.h"
-
+#include "main.h"
 /**
- *_strncpy - copy a string
- * @dest:input value
- * @src:input value
- * @n:input value
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  *
- * return:dest
+ * Return: dest
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
@@ -26,3 +24,4 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	return (dest);
 }
+
