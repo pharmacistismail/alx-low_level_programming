687a90f7375a8a923847f3a330becae351a529f1
pharmacistismail <pharmacist.77@outlook.com>
2023-09-21 00:16:39 -0700

    .

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,12 +11,12 @@
 char *string_toupper(char *n)
 {
 	int i;
-	
+
 	i = 0;
 	while (n[i] != '\0')
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
-			n[i] = n[i] - 32 ;
+			n[i] = n[i] - 32;
 		i++;
 	}
 	return (n);
