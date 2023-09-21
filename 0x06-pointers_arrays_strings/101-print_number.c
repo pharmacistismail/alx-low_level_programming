12253ae811a3f6ebdf2a0bdacf854ba70eded07d
pharmacistismail <pharmacist.77@outlook.com>
2023-09-21 16:36:57 -0700

    .

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,22 @@
 #include "main.h"
 
 /**
- * print_number - print numbers chars
- * @n: integer params
+ * print_number - print integer
+ * @n: integer to print
  * Return: 0
  */
 
 void print_number(int n)
 {
-	unsigned int n1;
-
-	n1 = n;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n1 = -n;
+		num = -num;
 	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+	_putchar((num % 10) + '0');
 }
