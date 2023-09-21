5e97bac3d7ba128244ba6ee60bb096c9ced1b034
pharmacistismail <pharmacist.77@outlook.com>
2023-09-20 19:49:35 -0700

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
-#include"main.h"
+#include "main.h"
 /**
- * _strcat-concatenates two strings
- *@dest:input value
- *@src:input value
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
  *
- * return:void
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
@@ -23,7 +23,8 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
-
+	
 	dest[i] = '\0';
 	return (dest);
 }
+
