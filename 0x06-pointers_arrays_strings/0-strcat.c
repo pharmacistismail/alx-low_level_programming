2264f7955d74354cdd85616b07fb2d06d8609a6e
pharmacistismail <pharmacist.77@outlook.com>
2023-09-20 19:43:59 -0700

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 /**
  * _strcat-concatenates two strings
  *@dest:input value
- * @src:input value
+ *@src:input value
  *
  * return:void
  */
@@ -23,6 +23,7 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
