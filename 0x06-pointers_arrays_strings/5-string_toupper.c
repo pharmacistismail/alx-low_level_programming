d82a131f4bc94b9f71d526b953a181335dded86e
pharmacistismail <pharmacist.77@outlook.com>
2023-09-21 00:24:57 -0700

    .

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,10 @@
-#include"main.h"
-
+#include "main.h"
 /**
- * string_toupper-change all lowercase to uppercase
- * @n:pointer
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
  *
- * return:n
- *
+ * Return: n
  */
-
 char *string_toupper(char *n)
 {
 	int i;
