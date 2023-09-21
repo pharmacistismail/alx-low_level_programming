16fc7c71dcbce80b32b9515f9bea9b3f21d2ea7f
pharmacistismail <pharmacist.77@outlook.com>
2023-09-21 13:38:41 -0700

    .

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers characters
+ * @n: integer parameters
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+
+	if (n < 0)
+	{
+		putchar('-');
+		n1 = -n;
+	}
+
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+
+	putchar((n1 % 10) + '0');
+}
+
