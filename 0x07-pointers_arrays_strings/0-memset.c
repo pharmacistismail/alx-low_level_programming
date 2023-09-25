2489c9ae87f821688f8b098b98b5cc3adbadcebd
pharmacistismail <pharmacist.77@outlook.com>
2023-09-25 08:05:48 -0700

    copy address

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,19 +3,19 @@
 /**
  * _memset - fill block of memory with specific value
  * @s: starting address of memory to be input
- * @w: wanted value
+ * @b: wanted value
  * @n: number of bytes need to be changed
  *
  * Return: changed array with new value for n bytes
 */
 
-char *_memset(char *s, char w, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
 	for (; n > 0; i++)
 	{
-		s[i] = w;
+		s[i] = b;
 		n--;
 	}
 	return (s);
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy -  function copies memory area
+ * @dest: the place where memory is stored
+ * @src: the place where memory is copied
+ * *@n: number of bytes
+ *
+ * Return: copied memory with n bytes changed
+ */
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
+
