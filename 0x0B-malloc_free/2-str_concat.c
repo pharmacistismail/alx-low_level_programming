cd3deb0b946939994804d6aac2ba9686ff05f923
pharmacistismail <pharmacist.77@outlook.com>
2023-10-04 02:42:23 -0700

    .

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *conct;
+	int l, ci;
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	
+	l = ci = 0;
+	while (s1[l] != '\0')
+		l++;
+	
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (l + ci + 1));
+	
+	if (conct == NULL)
+		return (NULL);
+	l = ci = 0;
+	while (s1[l] != '\0')
+	{
+		conct[l] = s1[l];
+		l++;
+	}
+	
+	while (s2[ci] != '\0')
+	{
+		conct[l] = s2[ci];
+		l++, ci++;
+	}
+	conct[l] = '\0';
+	return (conct);
+}
