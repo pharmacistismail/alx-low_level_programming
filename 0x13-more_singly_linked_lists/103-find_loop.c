d08d9a873e8ad23fc747c9eff1fc4c77b4069437
pharmacistismail <pharmacist.77@outlook.com>
2023-10-24 09:30:32 -0700

    .

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in linked list
+ * @head: linked list to be searched for
+ *
+ * Return: address of the node where the loop starts, or NULL
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *low = head;
+	listint_t *high = head;
+
+	if (!head)
+		return (NULL);
+
+	while (low && high && high->next)
+	{
+		high = high->next->next;
+		low = low->next;
+
+		if (high == low)
+		{
+			low = head;
+			while (low != high)
+			{
+				low = low->next;
+				high = high->next;
+			}
+			return (high);
+		}
+	}
+
+	return (NULL);
+}
